b71e136c6ae0b01474362b3ba1f530927841dd84
santiago <santiagob95@hotmail.com>
2021-05-24 00:40:36 -0300

    routes y schema de paises hecha. funca con postman :D

---
+++ models/paises.model.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+    const Paises = sequelize.define("paises", {
+      numero: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      nombreCorto: {
+        type: Sequelize.STRING
+      },
+      capital: {
+        type: Sequelize.STRING
+      },
+      nacionalidad: {
+        type: Sequelize.STRING
+      },
+      idiomas: {
+        type: Sequelize.STRING
+      },
+
+    },{
+        timestamps:false
+    });
+
+    return Paises;
+  };
